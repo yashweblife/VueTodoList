ae4139a48827620de696d0426635c0b269aa09ad
yashweblife <yashweblife20@gmail.com>
2022-11-08 03:33:16 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vue.todo.list',
+  appName: 'vue-todo-list',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
