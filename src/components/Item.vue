339eb5d06c8c7d56419e66821ec78c2b90a0a0b9
yashweblife <yashweblife20@gmail.com>
2022-11-09 01:49:07 -0600

    made a component

---
+++ src/components/Item.vue
@@ -0,0 +1,46 @@
+<template>
+    <ion-card :class="marked ? 'marked' : ''">
+        <ion-card-header>
+            <ion-toolbar>
+                <ion-title>{{data.title}}</ion-title>
+                <ion-buttons slot="end">
+                    <ion-button @click="markItem">Mark</ion-button>
+                    <ion-button @click="handleDelete">Clear</ion-button>
+                </ion-buttons>
+            </ion-toolbar>
+        </ion-card-header>
+        <ion-card-content>
+            {{data.info}}
+        </ion-card-content>
+    </ion-card>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name:'ItemVue',
+    props:["data"],
+    data:()=>{
+        return({
+            marked:false
+        })
+    },
+    methods:{
+        markItem(){
+            this.marked =!this.marked
+        },
+        handleDelete(){
+            this.$emit("delete-item",this.data.id)
+        }
+    }
+})
+</script>
+
+
+<style lang="scss" scoped>
+.marked{
+  opacity:0.3;
+  ion-title{
+    text-decoration: line-through;
+  }
+}
+</style>
