aa6ceb7e8808d9b52909d7d82bcd70de2cea83c4
yashweblife <yashweblife20@gmail.com>
2022-11-09 02:48:20 -0600

    Cleaner Code

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,26 +5,25 @@
         <ion-title>Vue Todo</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-fab vertical="bottom" horizontal="end">
-        <ion-fab-button>
+        <ion-fab-button @click="getInfo">
           <ion-icon name="add"></ion-icon>
         </ion-fab-button>
       </ion-fab>
       <ion-list>
-        <ItemVue v-for="item in list" :key="item.id" :data="item" @delete-item="handleDelete"/>
+        <ItemVue v-for="item in list" :key="item.id" :data="item" @delete-item="handleDelete" />
       </ion-list>
-    </ion-content>      
+    </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
 import ItemVue from '@/components/Item.vue';
-import {generateId, ItemInterface} from '../utils'
-
+import { alertController, IonContent, IonFab, IonFabButton, IonIcon, IonList, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { generateId, ItemInterface } from '../utils';
 export default defineComponent({
   name: 'HomePage',
   components: {
@@ -33,40 +32,75 @@ export default defineComponent({
     IonPage,
     IonTitle,
     IonToolbar,
-    ItemVue
+    ItemVue,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonList
   },
-  data:()=>{
-    const list:ItemInterface[] = [
+  data: () => {
+    const list: ItemInterface[] = [
       {
-        title:"Hello World",
-        info:"test",
-        id:generateId(),
-        marked:false
+        title: "Hello World",
+        info: "test",
+        id: generateId(),
+        marked: false
       },
       {
-        title:"World",
-        info:"test",
-        id:generateId(),
-        marked:false
+        title: "World",
+        info: "test",
+        id: generateId(),
+        marked: false
       },
       {
-        title:"Hello",
-        info:"test",
-        id:generateId(),
-        marked:false
+        title: "Hello",
+        info: "test",
+        id: generateId(),
+        marked: false
       },
     ]
-    return({
+    return ({
       list
     })
   },
-  methods:{
-    handleDelete(a:string){
-      let x = this.list.filter((item:ItemInterface)=>item.id!=a)
+  methods: {
+    handleDelete(a: string) {
+      let x = this.list.filter((item: ItemInterface) => item.id != a)
       this.list = x
     },
-    addItem(){
+    addItem() {
       this.list.push()
+    },
+    async getInfo() {
+      const alert = await alertController.create({
+        header: "Add a todo",
+        inputs: [
+          {
+            placeholder: "Enter A Name"
+          }, {
+            placeholder: "Enter some info"
+
+          }
+        ],
+        buttons: [
+          {
+            text: "Add",
+            handler: (val: any) => {
+              let data = {
+                title: val[0],
+                info: val[1],
+                marked: false,
+                id: generateId()
+              }
+              this.list.push(data)
+            }
+          },
+          {
+            text: "Discard"
+          }
+        ]
+      })
+      await alert.present()
     }
   }
 });
@@ -75,7 +109,7 @@ export default defineComponent({
 <style lang="scss" scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -91,14 +125,13 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
 #container a {
   text-decoration: none;
 }
-
 </style>
